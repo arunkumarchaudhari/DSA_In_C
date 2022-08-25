9a2e648f4865f7e0a8c258b284cc101ad73e973c
Arun Kumar Chaudhari <arunchaudhari12379@gmail.com>
2022-08-25 23:57:35 +0530

    Add files via upload

--- circularQueue_Using_CircularLinkedList.c
+++ circularQueue_Using_CircularLinkedList.c
@@ -1,112 +1,147 @@
+//Implementation of Circular Queue in Circualr Linekd List
 #include<stdio.h>
 #include<stdlib.h>
 
+//Creating ADT for circualr queue in Circular LinekdList
 struct node{
     int data;
     struct node *next;
 };
-
-typedef struct{
-    struct node *R;
+typedef struct {
+    //If we will see the structure of CircularQueue in Circular LinkedList
+    //We can see that in o(1) we can perofrm insertion and deletion if we know rear end of the queue.
+    //So no need of defining fron end..we can manage with rear end itself
+    struct node *rear; //Here r is Rearend
 }CQueue;
 
-int insert(CQueue *q, int v){
+//inint function to initialize rear
+void init(CQueue *cq) {
+    cq->rear=NULL;
+}
+
+//Insert function
+int insert(CQueue *cq,int value) {
     struct node *cur;
     cur=malloc(sizeof(struct node));
-    
-    if(cur==NULL) return 1;
-    cur->data=v;
 
-    if(q->R==NULL){
-        cur->next=cur;
-        q->R=cur;
+    //if cur not created properly
+    if(cur==NULL) return 1;
+
+    cur->next=NULL;
+    cur->data=value;
+
+    //No node present means rear is null.
+    if(cq->rear==NULL) {
+        cq->rear=cur;
+        cq->rear->next=cur;
     }
+    //Some node already present
     else{
-        cur->next=q->R->next;
-        q->R->next=cur;
-        q->R=cur;
+        cur->next=cq->rear->next; //assigning rear next i.e., front to cur next
+        cq->rear->next=cur; 
+        cq->rear=cur;
     }
     return 0;
 }
 
-int delete(CQueue *q, int *v){
+//Delete Function
+int delete(CQueue * cq,int *v) {
 
-    if(q->R==NULL){
-        printf("List is empty\n");
+    //if no node present
+    if(cq->rear==NULL){
+        printf("Queue is empty\n");
         return 1;
     }
-    if(q->R->next==q->R){
-        *v=q->R->data;
-        free(q->R);
-        q->R=NULL;
-    }
-    else{
-        struct node *ptr=q->R->next;
+
+    struct node *ptr;
+    ptr=cq->rear->next;
+    //only one node is present
+    if(cq->rear->next=cq->rear) {
         *v=ptr->data;
-        q->R->next=ptr->next;
+        cq->rear=NULL;
+        free(ptr);
+    }
+    //If more than one node present
+    else{
+        *v=ptr->data;
+        cq->rear->next=ptr->next;
         free(ptr);
     }
     return 0;
 }
 
-void init(CQueue *q){
-    q->R=NULL;
+//Display Function
+void display(CQueue *cq) {
+    printf("CQueue\t:");
+    if(cq->rear==NULL){
+        printf("");
+    }
+    else{
+        struct node *ptr;
+        ptr=cq->rear->next;
+        while(ptr!=cq->rear){
+            printf("%d ",ptr->data);
+            ptr=ptr->next;
+        }
+        printf("%d ",ptr->data);
+    }
+    printf("\n\n");
+    //NOTE: The reason behind using if else is that, when the list is empty,then also ptr is pointing to rear next which is null
+    //      due to which is it terminating program.
 }
-
 
 int main(){
     
     CQueue q1;
     init(&q1);
 
-    int data,choice,k;
+    int value,choice,k;
 
-    start:
-    printf("1. Insertion\n");
-    printf("2. Deletion\n");
-    printf("3. Display\n");
-    printf("4. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d",&choice);
+    while(1) {
+        printf("1. Insertion\n");
+        printf("2. Deletion\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
 
-    switch (choice)
-    {
-    case 1:
-        printf("Enter data to be inserted: ");
-        scanf("%d",&data);
-        k=insert(&q1,data);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data to be inserted: ");
+            scanf("%d",&value);
+            k=insert(&q1,value);
 
-        if(k==1) printf("Data Not inserted\n");
-        else printf("%d inserted successfully...",data);
-        printf("\n");
+            if(k==1) printf("Data Not inserted\n");
+            else printf("%d inserted successfully...",value);
+            printf("\n");
 
-        break;
-    case 2:
-        k=delete(&q1,&data);
-        if(k==0){
-            printf("Data Deleted Successfully\n");
-            printf("Deleted Data: %d",data);
+            break;
+        case 2:
+            k=delete(&q1,&value);
+            if(k==0){
+                printf("Data Deleted Successfully\n");
+                printf("Deleted Data: %d",value);
+            }
+            else{
+                printf("\nTry again\n");
+            }
+            printf("\n");
+            break;
+        case 3:
+            display(&q1);
+            break;
+
+        case 4:
+            printf("Program terminated successfully...\n");
+            exit(0);
+            break; //no need of break...automatically whole program will terminate.
+        
+        default:
+            printf("Enter valid choice\n");
+            break;
         }
-        else{
-            printf("\nTry again\n");
-        }
-        printf("\n");
-        break;
-    case 3:
-        display(&q1);
-        break;
-
-    case 4:
-        printf("Program terminated successfully...\n");
-        exit(0);
-        break; //no need of break...automatically whole program will terminate.
-    
-    default:
-        printf("Enter valid choice\n");
-        break;
     }
-
-    goto start;
 
 
     return 0;
